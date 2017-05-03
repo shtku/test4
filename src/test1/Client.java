59b1deabeb3f0bb3d69bfc801b297545fb6a234d
shtku <1137079045@qq.com>
2017-05-04 00:20:10 +0800

    sht

--- src/test1/Client.java
+++ src/test1/Client.java
@@ -7,7 +7,7 @@ import java.net.*;
 
 public class Client {
 	public static void main(String[] args) {
-		//没有用的
+		//没有用的rtrtrttr
 		String msg = "Client Date";
 		try{
 			Socket socket = new Socket("127.0.0.1",8080);
