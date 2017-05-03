d876c32130459e6d9160d0f5935d40d471567640
shtku <1137079045@qq.com>
2017-05-04 00:16:06 +0800

    添加注释

--- src/test1/Client.java
+++ src/test1/Client.java
@@ -7,6 +7,7 @@ import java.net.*;
 
 public class Client {
 	public static void main(String[] args) {
+		//没有用的
 		String msg = "Client Date";
 		try{
 			Socket socket = new Socket("127.0.0.1",8080);
